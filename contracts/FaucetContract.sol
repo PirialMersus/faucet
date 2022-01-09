cefd68becf820295e79e38185ff510283a1b4c10
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2022-01-09 16:08:26 +0200

    json -rps, first requests

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,4 +3,11 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
     receive() external payable {}
+
+    function addFunds() external payable {}
+
+    function justTest() external pure returns (uint256) {
+        return 2 + 2;
+    }
 }
+// const instance = await Faucet.deployed()
